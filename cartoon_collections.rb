1dc845399fc518109e7a62fbafa50661a61cbf64
Sherief Farid <sherief@farid.cloud>
2019-03-03 20:06:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i=0
+  while i < dwarves.length
+    dwarves.each_with_index do |dwarf, index|
+      puts "#{index+1}. #{dwarf}"
+    end
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
